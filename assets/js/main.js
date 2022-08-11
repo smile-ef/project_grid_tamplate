42c9a1b500bc4eb5a6221c834004637bb6c3facd
smile-ef <fedchishinae@gmail.com>
2022-08-11 22:10:27 +0300

    доработка кнопки

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,7 @@
 
-// $(document).ready(function () {
-
-//   $('.header-navbar__hamburger').on('click', function () {
-//     $(this).toggleClass('active');
-//     $('.header-navbar__list').toggleClass('active');
-//   });
-
-// });
 $(document).ready(function(){
 	$('.header-navbar__btn').click(function(){
 		$(this).toggleClass('header-navbar__btn--active');
+		$ ('.header-navbar__list').toggleClass('active')
 	});
 });
