2e0e38d87b3df48d589883fe619ba7581c0e6fe9
smile-ef <fedchishinae@gmail.com>
2022-08-11 21:36:28 +0300

    формление кнопки

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,14 @@
 
-$(document).ready(function () {
+// $(document).ready(function () {
 
-  $('.header-navbar__hamburger').on('click', function () {
-    $(this).toggleClass('active');
-    $('.header-navbar__list').toggleClass('active');
-  });
+//   $('.header-navbar__hamburger').on('click', function () {
+//     $(this).toggleClass('active');
+//     $('.header-navbar__list').toggleClass('active');
+//   });
 
+// });
+$(document).ready(function(){
+	$('.header-navbar__btn').click(function(){
+		$(this).toggleClass('header-navbar__btn--active');
+	});
 });
