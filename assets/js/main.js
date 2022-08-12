63c93b53be861fcb7ea502c3bceeaf2c73f608ef
smile-ef <fedchishinae@gmail.com>
2022-08-12 11:20:27 +0300

    окончание работы в классе

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,6 @@
 $(document).ready(function(){
 	$('.header-navbar__btn').click(function(){
 		$(this).toggleClass('header-navbar__btn--active');
-		$ ('.header-navbar__list').toggleClass('active')
+		$ ('.header-navbar__list').stop(true, true).slideToggle(500);
 	});
 });
